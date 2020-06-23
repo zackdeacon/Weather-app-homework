24174b036a091ac246eb6ab7a8d1f14130711054
Zachary Deacon <zackdeacon347@gmail.com>
2020-06-23 15:39:45 -0700

    finished 5 day forecast

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-//build bootstrap layout 
-//incoprorate weater app API to Ajax function 
 //put info pulled from weather app API into specific locations on page
-//create 5 day forecast div blocks with updating info 
+    //need to finish UV index 
+    //need to add icon next to city 
 //create ordered list with local storage for searched cities 
-//create search input system to search for cities 
 var currentDate = moment().format("MMM Do YYYY"); 
 
 
@@ -21,9 +19,10 @@ $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    //   console.log(response)
-
-    $("#city").append(" " + response.name + " " + currentDate);
+      console.log(response)
+    var cityImg = $("<img>");
+    cityImg.attr("src", "https://openweathermap.org/img/wn/" + response.weather[0].icon + ".png");
+    $("#city").append(" " + response.name + " " + currentDate + " " + cityImg);
     $("#temp").append(Math.floor(" " + response.main.temp));
     $("#humidity").append(" " + response.main.humidity + "%");
     $("#wind").append(" " + response.wind.speed + " MPH");
@@ -33,11 +32,14 @@ $.ajax({
     url: fiveDayForecast,
     method: "GET"
   }).then(function(data) {
-      console.log(data)
+    //   console.log(data)
         for (var i = 0; i < 5; i++ ) {
             var dateForecast = $("<p>");
             dateForecast.text(moment().add(i + 1, 'days').format("MMM Do YYYY"));
             $("#day" + i).append(dateForecast);
+            var newImg = $("<img>");
+            newImg.attr("src", "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png");
+            $("#day" + i).append(newImg);
             var newTemp = $("<p>");
             var tempForecast = data.list[i].main.temp;
             newTemp.text("Temp: " + Math.floor(tempForecast) + "F")
