517844294ada0e90c2ff10639b7e0c0a579ac54f
Zachary Deacon <zackdeacon347@gmail.com>
2020-06-23 19:40:03 -0700

    local storage started

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
-//put info pulled from weather app API into specific locations on page
-    //need to add icon next to city 
 //create ordered list with local storage for searched cities 
 var currentDate = moment().format("MMM Do YYYY"); 
+var searchHistory = [];
+var storedCities = JSON.parse(localStorage.getItem("searchHistory"));
 
-
-
-
+createButtons();
 $("#searchBtn").on("click", function() {
-
-    var city= $("#citySearch").val();
+    
+    var city= $("#citySearch").val().trim();
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" +
       city + "&units=imperial&APPID=119090b7f3bb3a2c44906450f645dd9a";
     var fiveDayForecast = "http://api.openweathermap.org/data/2.5/forecast?q=" +
     city + "&units=imperial&APPID=119090b7f3bb3a2c44906450f645dd9a"
 
+    if (!searchHistory.includes(city)) {
+        searchHistory.unshift(city);
+    }
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+createButtons();
 $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-      console.log(response)
+    //   console.log(response)
       var latitude = response.coord.lat;
       var longitude = response.coord.lon;
     var cityImg = $("<img>");
     cityImg.attr("src", "https://openweathermap.org/img/wn/" + response.weather[0].icon + ".png");
-    $("#city").append(" " + response.name + " " + currentDate + " " + cityImg);
-    $("#temp").append(Math.floor(" " + response.main.temp));
-    $("#humidity").append(" " + response.main.humidity + "%");
-    $("#wind").append(" " + response.wind.speed + " MPH");
+    $("#city-pic").empty().append(cityImg);
+    $("#city").empty().append(" " + response.name + " " + "(" + currentDate + ") ");
+    $("#temp").empty().append(" " + Math.floor(response.main.temp) + " F");
+    $("#humidity").empty().append(" " + response.main.humidity + "%");
+    $("#wind").empty().append(" " + response.wind.speed + " MPH");
     var uvIndex = "http://api.openweathermap.org/data/2.5/uvi?appid=119090b7f3bb3a2c44906450f645dd9a&" + "lat=" + latitude + "&lon=" + longitude ;
        //Ajax call to set UV index with colored span div 
     $.ajax({
@@ -36,7 +40,7 @@ $.ajax({
         //   console.log(uvResponse)
           var uvText = $("<span>");
           uvText.text(uvResponse.value);
-          $("#uv").append(uvText);
+          $("#uv").empty().append(uvText);
           if (uvResponse.value > 9) {
           uvText.attr("class", "high")
           } else if (uvResponse.value < 4) {
@@ -52,6 +56,7 @@ $.ajax({
   }).then(function(data) {
     //   console.log(data)
         for (var i = 0; i < 5; i++ ) {
+            $("#day" + i).empty();
             var dateForecast = $("<p>");
             dateForecast.text(moment().add(i + 1, 'days').format("MMM Do YYYY"));
             $("#day" + i).append(dateForecast);
@@ -60,7 +65,7 @@ $.ajax({
             $("#day" + i).append(newImg);
             var newTemp = $("<p>");
             var tempForecast = data.list[i].main.temp;
-            newTemp.text("Temp: " + Math.floor(tempForecast) + "F")
+            newTemp.text("Temp: " + Math.floor(tempForecast) + " F")
             $("#day" + i).append(newTemp);
             var newHumidity = $("<p>");
             var humidityForecast = data.list[i].main.humidity;
@@ -72,3 +77,17 @@ $.ajax({
   })
 
 })
+
+function createButtons () {
+    $("#searchResult").empty();
+    for (var i = 0; i < storedCities.length; i++) {
+        var newLi = $("<li>");
+        var newBtn = $("<button>");
+        newBtn.text(storedCities[i]);
+        newBtn.on("click", function(){
+            console.log("string")
+        })
+        newLi.append(newBtn);
+        $("#searchResult").append(newLi);
+    }
+}
